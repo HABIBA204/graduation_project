6a6e1873c960a0cc08fc25f2e81448486d418ec2
HABIBA204 <habibaebrahim204@gmail.com>
2026-09-19 17:18:52 +0300

    Add database models and basic admin configuration

---
+++ stock/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from .models import (
+    UserProfile, Supplier, Category, Product,
+    Sale, SaleItem, PurchaseOrder, PurchaseOrderItem
+)
+
+# الطريقة البسيطة والمباشرة تماماً:
+admin.site.register(UserProfile)
+admin.site.register(Supplier)
+admin.site.register(Category)
+admin.site.register(Product)
+admin.site.register(Sale)
+admin.site.register(SaleItem)
+admin.site.register(PurchaseOrder)
+admin.site.register(PurchaseOrderItem)
